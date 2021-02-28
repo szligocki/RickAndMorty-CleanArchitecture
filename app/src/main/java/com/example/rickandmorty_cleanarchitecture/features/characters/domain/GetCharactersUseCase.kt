a8d48eeeac8f5f999ed0de9451cefb76693817ad
szymonligocki <szymon.ligocki@whiteaster.com>
2021-03-01 00:17:39 +0100

    add Character

---
+++ app/src/main/java/com/example/rickandmorty_cleanarchitecture/features/characters/domain/GetCharactersUseCase.kt
@@ -0,0 +1,11 @@
+package com.example.rickandmorty_cleanarchitecture.features.characters.domain
+
+import com.example.rickandmorty_cleanarchitecture.core.base.UseCase
+import com.example.rickandmorty_cleanarchitecture.features.characters.CharacterRepository
+import com.example.rickandmorty_cleanarchitecture.features.characters.domain.model.Character
+
+class GetCharactersUseCase(private val characterRepository: CharacterRepository):
+    UseCase<List<Character>, Unit>() {
+
+    override suspend fun action(params: Unit): List<Character> = characterRepository.getCharacters()
+}
---
+++ app/src/test/java/com/example/rickandmorty_cleanarchitecture/features/characters/domain/GetCharactersUseCaseTest.kt
@@ -0,0 +1,26 @@
+package com.example.rickandmorty_cleanarchitecture.features.characters.domain
+
+import com.example.rickandmorty_cleanarchitecture.features.characters.CharacterRepository
+import io.mockk.coVerify
+import io.mockk.mockk
+import kotlinx.coroutines.GlobalScope
+import org.junit.Test
+
+internal class GetCharactersUseCaseTest {
+
+    @Test
+    fun `when use case is invoke than execute getCharacters method from repository`() {
+        //given
+        val repository = mockk<CharacterRepository>(relaxed = true)
+        val useCase = GetCharactersUseCase(repository)
+
+        //when
+        useCase(
+            params = Unit,
+            scope = GlobalScope
+        )
+
+        //than
+        coVerify { repository.getCharacters() }
+    }
+}
