c896d039681e2437b2b7823707628e1efafb8264
szymonligocki <szymon.ligocki@whiteaster.com>
2021-03-02 13:45:18 +0100

    create AppModule

---
+++ app/src/main/java/com/example/rickandmorty_cleanarchitecture/core/di/AppModule.kt
@@ -0,0 +1,18 @@
+package com.example.rickandmorty_cleanarchitecture.core.di
+
+import android.content.Context
+import android.net.ConnectivityManager
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.rickandmorty_cleanarchitecture.core.network.NetworkStateProvider
+import com.example.rickandmorty_cleanarchitecture.core.network.NetworkStateProviderImpl
+import org.koin.android.ext.koin.androidContext
+import org.koin.dsl.module
+
+val appModule = module {
+    factory<RecyclerView.LayoutManager> { LinearLayoutManager(androidContext()) }
+    factory { DividerItemDecoration(androidContext(), DividerItemDecoration.VERTICAL) }
+    factory { androidContext().getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager }
+    factory<NetworkStateProvider> { NetworkStateProviderImpl(get()) }
+}
