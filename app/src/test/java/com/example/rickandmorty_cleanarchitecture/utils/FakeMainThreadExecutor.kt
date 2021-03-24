61a5f7f7790d4dea3515666bd659876582593501
szymonligocki <szymon.ligocki@whiteaster.com>
2021-03-24 01:26:34 +0100

    added ViewModel Tests

--- app/src/test/java/com/example/rickandmorty_cleanarchitecture/utils/FakeMainThreadExecutor.kt
+++ app/src/test/java/com/example/rickandmorty_cleanarchitecture/utils/FakeMainThreadExecutor.kt
@@ -1,4 +1,12 @@
 package com.example.rickandmorty_cleanarchitecture.utils
 
-class FakeMainThreadExecutor {
+import androidx.arch.core.executor.TaskExecutor
+
+object FakeMainThreadExecutor : TaskExecutor() {
+
+    override fun executeOnDiskIO(runnable: Runnable) = runnable.run()
+
+    override fun postToMainThread(runnable: Runnable) = runnable.run()
+
+    override fun isMainThread(): Boolean = true
 }
--- app/src/test/java/com/example/rickandmorty_cleanarchitecture/utils/ViewModelTest.kt
+++ app/src/test/java/com/example/rickandmorty_cleanarchitecture/utils/ViewModelTest.kt
@@ -1,4 +1,18 @@
 package com.example.rickandmorty_cleanarchitecture.utils
 
-class ViewModelTest {
+import androidx.arch.core.executor.ArchTaskExecutor
+import org.junit.jupiter.api.AfterEach
+import org.junit.jupiter.api.BeforeEach
+
+open class ViewModelTest {
+
+    @BeforeEach
+    fun setUp(){
+        ArchTaskExecutor.getInstance().setDelegate(FakeMainThreadExecutor)
+    }
+
+    @AfterEach
+    fun tearDown(){
+        ArchTaskExecutor.getInstance().setDelegate(null)
+    }
 }
